bbf21028757b983f73069ff9de4b4ff37fbe9229
Iljamo Ilias <iljamo.ilias@qnips.io>
2019-12-13 15:28:24 +0100

    Proguard active

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,11 @@
 plugins {
     id("com.android.application")
     kotlin("android")
-    kotlin("android.extensions")
     kotlin("kapt")
 }
 
 android {
     compileSdkVersion(29)
-    buildToolsVersion = "29.0.2"
-
     defaultConfig {
         applicationId = "com.example.mytestapp"
         minSdkVersion(22)
@@ -21,6 +18,7 @@ android {
         getByName("debug") {
             isMinifyEnabled = true
             isShrinkResources = true
+            isUseProguard = true
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
         }
     }
@@ -31,17 +29,11 @@ android {
         }
     }
 
-    packagingOptions{
-        exclude("META-INF/DEPENDENCIES")
-        exclude("META-INF/metadata.jvm.kotlin_module")
-        exclude("META-INF/metadata.kotlin_module")
-    }
-
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk7", "1.3.61"))
-    implementation("androidx.appcompat:appcompat:1.1.0")
-    implementation("androidx.core:core-ktx:1.1.0")
-    implementation("com.squareup.moshi:moshi-kotlin-codegen:1.9.2")
+    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.9.2")
+    implementation("com.squareup.moshi:moshi:1.9.2")
+    implementation("androidx.annotation:annotation:1.1.0")
 }
--- app/src/main/kotlin/com/example/mytestapp/MainActivity.kt
+++ app/src/main/kotlin/com/example/mytestapp/MainActivity.kt
@@ -1,32 +1,23 @@
 package com.example.mytestapp
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
 import com.squareup.moshi.Moshi
-import kotlinx.android.synthetic.main.activity_main.*
 import okio.Okio
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
-
-        var counter = 0
         val testProduct = Moshi.Builder().build().adapter(TestProduct::class.java)
             .fromJson(Okio.buffer(Okio.source(resources.openRawResource(R.raw.product))))
             ?: throw AssertionError()
         val nutritionFacts = testProduct.NutritionFacts ?: throw  IllegalArgumentException()
 
-        click.setOnClickListener {
-            result.text =
-                if (nutritionFacts.KCalPer100 != null && nutritionFacts.KJPer100 != null) {
-                    nutritionFacts.getValue(resources, 0, true)
-                } else {
-                    "Result. Attempt: ${counter++}"
-                }
+        if (nutritionFacts.KCalPer100 != null && nutritionFacts.KJPer100 != null) {
+            Log.e("Tag", nutritionFacts.getValue(resources, 0, true))
         }
     }
-
 }
